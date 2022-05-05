ff9dfb0cabad3e6f6caf2456cc78e9ea184904ca
RASHIJAIN <rashi.jain@celloip.com>
2022-05-05 10:02:54 +0530

    code

--- script.js
+++ script.js
@@ -100,71 +100,7 @@ var lastUrl = '';
 var totalPages = 100;
 var showData = []
 
-// var selectedGenre = []
-// setGenre();
-// function setGenre() {
-//     tagsEl.innerHTML= '';
-//     genres.forEach(genre => {
-//         const t = document.createElement('div');
-//         t.classList.add('tag');
-//         t.id=genre.id;
-//         t.innerText = genre.name;
-//         t.addEventListener('click', () => {
-//             if(selectedGenre.length == 0){
-//                 selectedGenre.push(genre.id);
-//             }else{
-//                 if(selectedGenre.includes(genre.id)){
-//                     selectedGenre.forEach((id, idx) => {
-//                         if(id == genre.id){
-//                             selectedGenre.splice(idx, 1);
-//                         }
-//                     })
-//                 }else{
-//                     selectedGenre.push(genre.id);
-//                 }
-//             }
-//             console.log(selectedGenre)
-//             getMovies(API_URL + '&with_genres='+encodeURI(selectedGenre.join(',')))
-//             highlightSelection()
-//         })
-//         tagsEl.append(t);
-//     })
-// }
 
-function highlightSelection() {
-    const tags = document.querySelectorAll('.tag');
-    tags.forEach(tag => {
-        tag.classList.remove('highlight')
-    })
-    clearBtn()
-    if(selectedGenre.length !=0){   
-        selectedGenre.forEach(id => {
-            const hightlightedTag = document.getElementById(id);
-            hightlightedTag.classList.add('highlight');
-        })
-    }
-
-}
-
-function clearBtn(){
-    let clearBtn = document.getElementById('clear');
-    if(clearBtn){
-        clearBtn.classList.add('highlight')
-    }else{
-            
-        let clear = document.createElement('div');
-        clear.classList.add('tag','highlight');
-        clear.id = 'clear';
-        clear.innerText = 'Clear x';
-        clear.addEventListener('click', () => {
-            selectedGenre = [];
-            setGenre();            
-            getMovies(API_URL);
-        })
-        tagsEl.append(clear);
-    }
-    
-}
 
 getMovies(API_URL);
 
@@ -221,63 +157,9 @@ function showMovies(data) {
         `
 
         main.appendChild(movieEl);
-        // document.getElementById(id).addEventListener('click', () => {
-        //   console.log(id)
-        //   openNav(movie)
-        // })
     })
 }
 
-const overlayContent = document.getElementById('overlay-content');
-/* Open when someone clicks on the span element */
-function openNav(movie) {
-  let id = movie.id;
-  fetch(BASE_URL + '/movie/'+id+'/videos?'+API_KEY).then(res => res.json()).then(videoData => {
-    console.log(videoData);
-    if(videoData){
-      document.getElementById("myNav").style.width = "100%";
-      if(videoData.results.length > 0){
-        var embed = [];
-        var dots = [];
-        videoData.results.forEach((video, idx) => {
-          let {name, key, site} = video
-
-          if(site == 'YouTube'){
-              
-            embed.push(`
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          
-          `)
-
-            dots.push(`
-              <span class="dot">${idx + 1}</span>
-            `)
-          }
-        })
-        
-        var content = `
-        <h1 class="no-results">${movie.original_title}</h1>
-        <br/>
-        
-        ${embed.join('')}
-        <br/>
-        <div class="dots">${dots.join('')}</div>
-        
-        `
-        overlayContent.innerHTML = content;
-        activeSlide=0;
-        showVideos();
-      }else{
-        overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
-      }
-    }
-  })
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
 
 var activeSlide = 0;
 var totalVideos = 0;
